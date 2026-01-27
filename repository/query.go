33ca699625437890ffa918fb0267ef4b7ca14f01
mogryo <tad.baikstis@gmail.com>
2026-01-27 20:24:01 +0200

    Add version check before read/write db operations

--- repository/query.go
+++ repository/query.go
@@ -11,6 +11,7 @@ import (
 )
 
 func (r *Repository) FindSnippetTags(ctx context.Context, snippetId int64) []gwkeitdb.Tag {
+	r.checkVersion(ctx)
 	tags, err := r.queries.FindTagsBySnippetId(ctx, snippetId)
 
 	if err != nil {
@@ -21,6 +22,7 @@ func (r *Repository) FindSnippetTags(ctx context.Context, snippetId int64) []gwk
 }
 
 func (r *Repository) FindSnippetUrls(ctx context.Context, snippetId int64) []gwkeitdb.Url {
+	r.checkVersion(ctx)
 	urls, err := r.queries.FindUrlsBySnippetId(ctx, snippetId)
 
 	if err != nil {
@@ -31,6 +33,7 @@ func (r *Repository) FindSnippetUrls(ctx context.Context, snippetId int64) []gwk
 }
 
 func (r *Repository) FindSnippetsByTags(ctx context.Context, tags []string) []gwkeitdb.Snippet {
+	r.checkVersion(ctx)
 	snippets, err := r.queries.FindSnippetsByTags(ctx, tags)
 
 	if err != nil {
@@ -41,6 +44,7 @@ func (r *Repository) FindSnippetsByTags(ctx context.Context, tags []string) []gw
 }
 
 func (r *Repository) FindSnippetsByLikeTags(ctx context.Context, tags []string) []gwkeitdb.Snippet {
+	r.checkVersion(ctx)
 	allSnippets := make([]gwkeitdb.Snippet, 0)
 	for _, tag := range tags {
 		snippets, err := r.queries.FindSnippetsByLikeTags(ctx, "%"+tag+"%")
@@ -55,6 +59,7 @@ func (r *Repository) FindSnippetsByLikeTags(ctx context.Context, tags []string) 
 }
 
 func (r *Repository) FindSnippetsByFts(ctx context.Context, tags []string) []gwkeitdb.Snippet {
+	r.checkVersion(ctx)
 	prefixTags := slicelib.Map(tags, func(tag string) string { return tag + "*" })
 	rows, err := r.db.QueryContext(
 		ctx,
@@ -93,6 +98,8 @@ func (r *Repository) FindSnippetsByFts(ctx context.Context, tags []string) []gwk
 }
 
 func (r *Repository) FindSnippetsByPage(ctx context.Context, page int64, size int64) ([]gwkeitdb.Snippet, error) {
+	r.checkVersion(ctx)
+
 	if page <= 0 {
 		return nil, errors.New("page must be greater than 0")
 	}
@@ -104,6 +111,7 @@ func (r *Repository) FindSnippetsByPage(ctx context.Context, page int64, size in
 }
 
 func (r *Repository) FindSnippet(ctx context.Context, id int64) gwkeitdb.FindSnippetDataByIdRow {
+	r.checkVersion(ctx)
 	snippet, err := r.queries.FindSnippetDataById(ctx, id)
 	if err != nil {
 		panic(err)
@@ -113,6 +121,7 @@ func (r *Repository) FindSnippet(ctx context.Context, id int64) gwkeitdb.FindSni
 }
 
 func (r *Repository) GetSnippetCount(ctx context.Context) int64 {
+	r.checkVersion(ctx)
 	count, err := r.queries.GetSnippetCount(ctx)
 	if err != nil {
 		panic(err)
--- repository/repository.go
+++ repository/repository.go
@@ -1,19 +1,57 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/gwkeit/gwkeitdb"
 )
 
 type Repository struct {
-	db      *sql.DB
-	queries *gwkeitdb.Queries
+	db            *sql.DB
+	queries       *gwkeitdb.Queries
+	activeVersion int64
 }
 
 func New(
 	db *sql.DB,
 	queries *gwkeitdb.Queries,
 ) *Repository {
-	return &Repository{db: db, queries: queries}
+	activeVersion := getLatestAppliedVersion(context.Background(), db)
+	return &Repository{db: db, queries: queries, activeVersion: activeVersion}
+}
+
+func (r *Repository) checkVersion(ctx context.Context) {
+	version := getLatestAppliedVersion(ctx, r.db)
+	if version != r.activeVersion {
+		panic("Database version mismatch")
+	}
+}
+
+func getLatestAppliedVersion(ctx context.Context, db *sql.DB) int64 {
+	rows, err := db.QueryContext(
+		ctx,
+		"SELECT max(version_id) FROM goose_db_version WHERE is_applied = 1;",
+	)
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(rows)
+	if err != nil {
+		panic(err)
+	}
+
+	if rows == nil || !rows.Next() {
+		panic("No applied version found")
+	}
+
+	var versionId int64
+	err = rows.Scan(&versionId)
+
+	if err != nil {
+		panic(err)
+	}
+	return versionId
 }
