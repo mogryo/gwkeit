64811e10e6a727b7070ee28630840dd93f95ca33
mogryo <tad.baikstis@gmail.com>
2026-04-05 17:52:05 +0300

    PR coderabbit comments

--- migrations/00004_snippets_tags_unique.sql
+++ migrations/00004_snippets_tags_unique.sql
@@ -1,6 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 
+DELETE FROM snippets_tags WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM snippets_tags
+    GROUP BY snippet_id, tag_id
+);
 CREATE UNIQUE INDEX idx_snippet_tag ON snippets_tags(snippet_id, tag_id);
 
 -- +goose StatementEnd
